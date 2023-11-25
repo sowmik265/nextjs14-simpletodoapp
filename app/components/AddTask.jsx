bd72647583f2564cce776c786684d55a1ed5833d
Tanvir Ahamed Sowmik <111014007+sowmik265@users.noreply.github.com>
2023-11-25 17:01:03 +0600

    modal created

--- app/components/AddTask.jsx
+++ app/components/AddTask.jsx
@@ -1,11 +1,44 @@
+"use client";
+import { useState } from "react";
 import { FaPlusCircle } from "react-icons/fa";
+import Modal from "./Modal";
 
 const AddTask = () => {
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const [newTaskValue, setNewTaskValue] = useState("");
+
+  const handleSubmitNewTodo = (e) => {
+    e.preventDefault();
+    console.log(newTaskValue);
+    setNewTaskValue("");
+  };
+
   return (
     <div>
-      <button className="btn btn-primary w-full">
-        Add New Task <FaPlusCircle size={18} />
+      <button
+        onClick={() => setModalOpen(true)}
+        className="btn btn-primary w-full"
+      >
+        ADD NEW TASK <FaPlusCircle size={18} />
       </button>
+      <Modal modalOpen={modalOpen} setModalOpen={setModalOpen}>
+        <form onSubmit={handleSubmitNewTodo}>
+          <h3 className=" font-bold text-lg">Add New Task</h3>
+          <div className="modal-action">
+            <input
+              value={newTaskValue}
+              onChange={(e) => setNewTaskValue(e.target.value)}
+              type="text"
+              placeholder="Type here"
+              className="input input-bordered w-full "
+            />
+            <button type="submit" className="btn">
+              SUBMIT
+            </button>
+          </div>
+        </form>
+      </Modal>
     </div>
   );
 };
